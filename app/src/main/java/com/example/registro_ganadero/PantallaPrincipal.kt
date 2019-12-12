334ea61b62e346a42573b75b91c14e5a00ee887f
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2019-12-12 06:50:48 -0600

    Terminando Funcionabilidad Botones....

--- app/src/main/java/com/example/registro_ganadero/PantallaPrincipal.kt
+++ app/src/main/java/com/example/registro_ganadero/PantallaPrincipal.kt
@@ -1,12 +1,17 @@
 package com.example.registro_ganadero
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class PantallaPrincipal : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_principal)
+    }
+    fun agregarganado(view: View){
+        startActivity(Intent(this, RegistroGanado::class.java))
     }
 }
